eeb8a44969bf6f9589ce47dcb4bb082c285573b4
Victor Shamanov <wiruzx@gmail.com>
2015-05-26 00:38:47 +0300

    Add dumped XCode headers

---
+++ XJump/XJump-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  XJump-Bridging-Header.h
+//  XJump
+//
+//  Created by Victor Shamanov on 5/26/15.
+//  Copyright (c) 2015 Victor Shamanov. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+#import "DVTFoundation.h"
+#import "DVTKit.h"
+#import "IDEFoundation.h"
+#import "IDEKit.h"
+#import "IDESourceEditor.h"
